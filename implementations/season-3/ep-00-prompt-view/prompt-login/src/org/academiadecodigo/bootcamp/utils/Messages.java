5b6354cf8657fd25a115be5c67602676a08a632d
Maria Luz <maria.luz@academiadecodigo.org>
2022-06-29 13:49:21 +0100

    added prompt-login

---
+++ implementations/season-3/ep-00-prompt-view/prompt-login/src/org/academiadecodigo/bootcamp/utils/Messages.java
@@ -0,0 +1,13 @@
+package org.academiadecodigo.bootcamp.utils;
+
+public class Messages {
+
+    public final static String STARTUP_MSG = "Welcome to your first Prompt-View Experience!";
+
+    public final static String LOGIN_MSG = "Insert your login credentials below:";
+    public static final String LOGIN_ERROR = "Username and/or password incorrect.";
+    public static final String LOGIN_SUCCESS = "Login Successful!";
+
+    public static final String WELCOME_MSG = "Welcome home, ";
+
+}
